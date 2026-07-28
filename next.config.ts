c0c14703cf72f4baf71cb69d4f5db6d266ac57ef
Z User <z@container>
2026-07-28 22:39:33 +0000

    d9562d51-d364-4186-b4e0-8bc81c95b369

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/ragia-realestate",
+  assetPrefix: "/ragia-realestate/",
   images: {
     unoptimized: true,
   },
