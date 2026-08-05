086feb96aa22f120c3fb1ead94ef4a10b3c2ffcf
Mazen mohamed <mazoonvc@gmail.com>
2026-08-05 20:40:19 +0400

    Remove basePath and assetPrefix from config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ragia-realestate",
-  assetPrefix: "/ragia-realestate/",
   images: {
     unoptimized: true,
   },
