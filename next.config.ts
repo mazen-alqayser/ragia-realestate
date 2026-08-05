768a7b0f06c0ab0220698aef2c2f2f80590285a8
Mazen mohamed <mazoonvc@gmail.com>
2026-08-05 22:06:04 +0400

    Refactor next.config.ts for consistency

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ragia-realestate", // ضروري جداً لـ GitHub Pages
-  trailingSlash: true, // يساعد في حل مشكلة 404 عند التنقل بين الصفحات
+  basePath: "/ragia-realestate",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
