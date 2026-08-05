8a3df43fe1283b66bf9ba499f23c57ced0e53654
Mazen mohamed <mazoonvc@gmail.com>
2026-08-05 22:04:49 +0400

    Configure base path and trailing slashes in next.config
    Set base path for GitHub Pages and enable trailing slashes.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/ragia-realestate", // ضروري جداً لـ GitHub Pages
+  trailingSlash: true, // يساعد في حل مشكلة 404 عند التنقل بين الصفحات
   images: {
     unoptimized: true,
   },
